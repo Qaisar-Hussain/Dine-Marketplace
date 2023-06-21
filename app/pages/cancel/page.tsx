fe75f0e9b875dd78f2dd624e33cd3ba7340d1188
qaisarhussain <qhussain960@gmail.com>
2023-06-21 17:47:39 +0500

    success and fail urls

---
+++ app/pages/cancel/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='flex justify-center'>
+      <p>your paayment is failed. Please rechecck your card detail and try again.</p>
+    </div>
+  )
+}
---
+++ app/pages/success/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='flex justify-center'>
+      <p>your paayment is successfully collected yoe will soon receive an email.</p>
+    </div>
+  )
+}
